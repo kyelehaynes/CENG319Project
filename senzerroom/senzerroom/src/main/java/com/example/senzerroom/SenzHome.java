0618d82b3eb833991b36ff6ace3114d322c0be54
sam9dadet <sam9dadet@github.com>
2018-09-27 19:54:48 -0400

    added login screen
    just started the login screen yet to be completed

---
+++ senzerroom/senzerroom/src/main/java/com/example/senzerroom/SenzHome.java
@@ -0,0 +1,63 @@
+package com.example.senzerroom;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.Image;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Switch;
+
+public class SenzHome extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.senzmenu, menu);
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_senz_home);
+
+        ImageView room1 =(ImageView) findViewById(R.id.imageView2);
+        ImageView room2 =(ImageView) findViewById(R.id.imageView3);
+        ImageView room3 =(ImageView) findViewById(R.id.imageView4);
+
+
+        room1.setOnClickListener(this);
+        room2.setOnClickListener(this);
+        room3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent intent1 = new Intent(SenzHome.this, SenzRoom1.class);
+        Intent intent2 = new Intent(SenzHome.this, SenzRoom2.class);
+        Intent intent3 = new Intent(SenzHome.this, SenzRoom3.class);
+
+
+        switch(v.getId()){
+
+            case R.id.imageView2:
+                startActivity(intent1);
+                break;
+            case R.id.imageView3:
+                startActivity(intent2);
+                break;
+
+            case R.id.imageView4:
+                startActivity(intent3);
+                break;
+        }
+    }
+}
